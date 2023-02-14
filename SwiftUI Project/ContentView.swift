5a81a1a8ef162a9021ebb50355c12228bf4eaa99
SaintStranger <Anton-bopoh@yandex.ru>
2023-02-14 07:54:53 +0300

    Initial Commit

---
+++ SwiftUI Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Project
+//
+//  Created by test on 14.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Project/SwiftUI_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ProjectApp.swift
+//  SwiftUI Project
+//
+//  Created by test on 14.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
